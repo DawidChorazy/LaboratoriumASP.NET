5a568722caf2f05ab575701f2f4ccf07aa8767a5
dawid.chorazy <dawid.chorazy@microsoft.wsei.edu.pl>
2024-10-21 18:38:34 +0200

    init

---
+++ WebApp/Controllers/BirthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers;
+
+public class BirthController : Controller
+{
+    public IActionResult Form()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Result(Birth model)
+    {
+        if (!model.IsValid())
+        {
+            return View("Error");
+        }
+
+        int age = model.CalculateAge();
+        ViewBag.Message = $"Cześć {model.Name}, masz {age} lat(a).";
+        return View();
+    }
+}
