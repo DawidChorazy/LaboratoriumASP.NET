24de6c07f8d428966d0c6f2c45eea087302499c0
dawid.chorazy <dawid.chorazy@microsoft.wsei.edu.pl>
2024-10-22 16:31:39 +0200

    init

--- WebApp/Models/ContactModel.cs
+++ WebApp/Models/ContactModel.cs
@@ -9,10 +9,24 @@ public class ContactModel
 {
     [HiddenInput]
     public int Id { get; set; }
-    [Required()]
+
+    [Required(ErrorMessage = "Please, type in your name!")]
+    [MaxLength(length: 20, ErrorMessage = "Name cannot be longer than 20 characters!")]
+    [MinLength(length: 1, ErrorMessage = "Name cannot be shorter than 1 character!")]
     public string FirstName { get; set; }
+
+    [Required(ErrorMessage = "Please, type in your name!")]
+    [MaxLength(length: 50, ErrorMessage = "Last name cannot be longer than 20 characters!")]
+    [MinLength(length: 1, ErrorMessage = "Last name cannot be shorter than 1 character!")]
     public string LastName { get; set; }
+    
+    [EmailAddress]
     public string Email { get; set; }
+    
+    [DataType(DataType.Date)]
     public DateOnly Birth { get; set; }
+    
+    [Phone]
+    [RegularExpression("\\+\\d\\d \\d\\d\\d \\d\\d\\d \\d\\d\\d", ErrorMessage = "Enter number: +xx xxx xxx xxx")]
     public string PhoneNumber { get; set; }
 }
