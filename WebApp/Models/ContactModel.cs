d5791d33b706cfd48c6846a0d333ca4cb8a6f79f
dawid.chorazy <dawid.chorazy@microsoft.wsei.edu.pl>
2024-10-22 15:38:41 +0200

    init

---
+++ WebApp/Models/ContactModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices.JavaScript;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+namespace WebApp.Models;
+
+public class ContactModel
+{
+    [HiddenInput]
+    public int Id { get; set; }
+    [Required()]
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public DateOnly Birth { get; set; }
+    public string PhoneNumber { get; set; }
+}
